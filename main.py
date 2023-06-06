49b2c9cb456bd9c65b03063f942d6eaeecc76ebe
greg0910 <gregorito35@gmail.com>
2023-06-06 10:59:02 -0500

    prueba

--- main.py
+++ main.py
@@ -5,29 +5,47 @@ import RPi.GPIO as GPIO
 from subprocess import check_call
 import time
 
-GPIO.setmode(GPIO.BCM)
 
-sensor = Adafruit_DHT.DHT11
-pin_sensor_hum = 18
-pin_apagar_raspi = 27
-pin_leds = 22
-red = LED(23)
-GPIO.setup(pin_leds, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+class Humificador:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.OUT)
+
+    def encender(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def apagar(self):
+        GPIO.output(self.pin, GPIO.LOW)
+
+
+class TiraLED:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.OUT)
+
+    def encender(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def apagar(self):
+        GPIO.output(self.pin, GPIO.LOW)
 
 
 class SistemaCultivo:
-    def __init__(self):
-        self.boton_apagar_rap = Button(pin_apagar_raspi, hold_time=3)
-        self.boton_apagar_rap.when_held = self.apagar_rap
-        GPIO.add_event_detect(pin_leds, GPIO.BOTH, callback=self.boton_leds_apagar_y_prender, bouncetime=200)
+    def __init__(self, pin_shutdown, btn_leds, sensor, pin_sensor_hum):
+        self.btn_shutdown = Button(pin_shutdown, hold_time=3)
+        self.btn_shutdown.when_held = self.apagar_rap
+        GPIO.add_event_detect(
+            btn_leds, GPIO.BOTH, callback=self.switch_tira_leds, bouncetime=200)  # bounce time para evitar rebotes
+        self.sensor = sensor
+        self.pin_sensor_hum = pin_sensor_hum
 
     def sensor_humytemp(self):
-        hum, temp = Adafruit_DHT.read_retry(sensor, pin_sensor_hum)
+        hum, temp = Adafruit_DHT.read_retry(self.sensor, self.pin_sensor_hum)
         return hum, temp
 
-    def boton_leds_apagar_y_prender(self, channel):
+    def switch_tira_leds(self, channel):
         # Lógica para apagar o prender los LEDs
-        if GPIO.input(pin_leds):
+        if GPIO.input(btn_leds):
             red.on()
         else:
             red.off()
@@ -43,5 +61,13 @@ class SistemaCultivo:
             time.sleep(0.1)
 
 
-sistema_cultivo = SistemaCultivo()
+GPIO.setmode(GPIO.BCM)
+sensor = Adafruit_DHT.DHT11
+pin_sensor_hum = 18
+pin_shutdown = 27
+btn_leds = 22
+red = LED(23)
+GPIO.setup(btn_leds, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+sistema_cultivo = SistemaCultivo(pin_shutdown, btn_leds, sensor, pin_sensor_hum)
 sistema_cultivo.ejecutar()
